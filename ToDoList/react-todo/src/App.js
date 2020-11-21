762dff2539ec7619d0bbd951acc13d7ff1c2dd51
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2020-11-21 18:16:32 +0100

    feat: React ToDoList app

---
+++ ToDoList/react-todo/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import './App.css';
+import Todo from './components/Todo';
+import Form from './components/Form';
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      key={task.id}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  return (
+    <div className='todoapp stack-large'>
+      <Form addTask={addTask} />
+      <h2 id='list-heading'>{headingText}</h2>
+      <ul
+        className='todo-list stack-large stack-exception'
+        aria-labelledby='list-heading'
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ ToDoList/react-todo/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+const DATA = [
+  { id: 'todo-0', name: 'Eat' },
+  { id: 'todo-1', name: 'Sleep' },
+  { id: 'todo-2', name: 'Repeat' }
+];
+
+ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
