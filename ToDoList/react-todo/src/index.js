ca6acf7ec342169fd4d81d2f9bb13d7df40b4d68
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2020-11-22 13:33:18 +0100

    Code cleanup - react app

--- ToDoList/react-todo/src/index.js
+++ ToDoList/react-todo/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
 const tasks = localStorage.getItem('tasks')
   ? JSON.parse(localStorage.getItem('tasks'))
@@ -11,12 +10,4 @@ const tasks = localStorage.getItem('tasks')
 
 ReactDOM.render(<App tasks={tasks} />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
